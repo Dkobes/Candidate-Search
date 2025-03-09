e9d1c0c3cf019e2e6eb78616638f786d2f922062
Danyelle <dkobes15@gmail.com>
2025-03-09 14:05:42 -0500

    working on save button

--- src/pages/SavedCandidates.tsx
+++ src/pages/SavedCandidates.tsx
@@ -19,44 +19,43 @@ const SavedCandidates = () => {
 
   return (
     <>
-      {JSON.stringify(candidates) === '[]' ? (
-        <div style={{backgroundColor: 'black', borderRadius: 25, textAlign: 'center'}}>
+      {candidates.length === 0 ? (
+        <div style={{ backgroundColor: 'black', borderRadius: 25, textAlign: 'center' }}>
           <h2>No Candidates Saved</h2>
         </div>
       ) : (
         <>
           <h1>Potential Candidates</h1>
           <table className="table">
-  <thead>
-    <tr>
-      <th scope="col">Image</th>
-      <th scope="col">Name</th>
-      <th scope="col">Location</th>
-      <th scope="col">Email</th>
-      <th scope="col">Company</th>
-      <th scope="col">Reject</th>
-    </tr>
-  </thead>
-  <tbody>
-    {candidates.map((candidate: Candidate) => (
-      <tr key={candidate.id}>
-        <td><img src={candidate.avatar_url} alt={`${candidate.name}'s avatar`} /></td>
-        <td>
-          <h3>{candidate.name}</h3>
-          <h3>({candidate.username})</h3>
-        </td>
-        <td>{candidate.location}</td>
-        <td>{candidate.email}</td>
-        <td>{candidate.company}</td>
-        <td><button onClick={() => reject(candidate.id)}>Reject</button></td>
-      </tr>
-    ))}
-  </tbody>
-</table>
-      </>
+            <thead>
+              <tr>
+                <th scope="col">Image</th>
+                <th scope="col">Name</th>
+                <th scope="col">Location</th>
+                <th scope="col">Email</th>
+                <th scope="col">Company</th>
+                <th scope="col">Reject</th>
+              </tr>
+            </thead>
+            <tbody>
+              {candidates.map(candidate => (
+                <tr key={candidate.id}>
+                  <td><img src={candidate.avatar_url} alt={`${candidate.name}'s avatar`} /></td>
+                  <td><h3>{candidate.name}</h3><h3>({candidate.username})</h3></td>
+                  <td>{candidate.location}</td>
+                  <td>{candidate.email}</td>
+                  <td>{candidate.company}</td>
+                  <td>{candidate.bio}</td>
+                  <td><button onClick={() => reject(candidate.id)}>Reject</button></td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </>
       )}
     </>
   );
 };
+
 
 export default SavedCandidates;
