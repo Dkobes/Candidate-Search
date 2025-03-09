d708bdf91e2bd8ced87efacac27929b9e797f2a2
Danyelle <dkobes15@gmail.com>
2025-03-08 19:30:34 -0600

    Trying Nav bar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
 const Nav = () => {
   // TODO: Add necessary code to display the navigation bar and link between the pages
   return (
-    <div>Nav</div>
+    <nav>
+    <ul>
+      <li>
+        <Link to="/">Candidate Search</Link>
+      </li>
+      <li>
+        <Link to="/saved-candidates">Saved Candidates</Link>
+      </li>
+    </ul>
+  </nav>
   )
 };
 
